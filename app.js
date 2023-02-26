e3d8d45673933ebd8fad08b28bc845665fc571ad
“[Martialeagle <k.l.ashwanthram@gmail.com>
2023-02-26 16:15:25 +0530

    added random generation of colors array

--- app.js
+++ app.js
@@ -3,13 +3,17 @@ const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
 var button = document.querySelector('#btn');
 var bkcolor = document.querySelector('.bkcolor')
 button.addEventListener('click', changeColor);
-var iterator = 0;
-
 
 function changeColor(){
-    var color = colors[iterator%4]
+    var color = colors[Math.round(colorgenerator())%4]
+    // console.log(Math.round(colorgenerator()));
     document.body.style.backgroundColor = color;
-    console.log(color);
+    // console.log(color);
     bkcolor.innerHTML = color;
-    iterator++;
+}
+
+function colorgenerator(){
+    var num = Math.random()* colors.length;
+    return num;
+    // console.log(num);
 }
